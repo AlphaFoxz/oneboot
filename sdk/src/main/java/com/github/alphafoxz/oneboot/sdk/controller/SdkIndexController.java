550cfb86aaa5c071dce99ee6f400e0cfb36495fd
EricFox <841958335@qq.com>
2023-08-19 16:10:51 +0800

    整合SpringDoc、预留restful路径

--- sdk/src/main/java/com/github/alphafoxz/oneboot/sdk/controller/SdkIndexController.java
+++ sdk/src/main/java/com/github/alphafoxz/oneboot/sdk/controller/SdkIndexController.java
@@ -1,9 +1,11 @@
 package com.github.alphafoxz.oneboot.sdk.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "SdkInfoController", description = "Sdk导航页面")
 @RestController
 @RequestMapping("/_sdk")
 public class SdkIndexController {
--- sdk/src/main/java/com/github/alphafoxz/oneboot/sdk/controller/SdkInfoController.java
+++ sdk/src/main/java/com/github/alphafoxz/oneboot/sdk/controller/SdkInfoController.java
@@ -2,12 +2,14 @@ package com.github.alphafoxz.oneboot.sdk.controller;
 
 import com.github.alphafoxz.oneboot.sdk.SdkConstants;
 import com.github.alphafoxz.oneboot.sdk.service.SdkInfoService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "SdkInfoController", description = "Sdk模块基本信息接口")
 @RestController
 @RequestMapping("/_sdk/info")
 public class SdkInfoController {
