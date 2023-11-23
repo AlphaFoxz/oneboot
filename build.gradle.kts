5baa558ce5368aa04b8c36636b1e77317e6748ed
AlphaFoxz <841958335@qq.com>
2023-11-23 17:48:34 +0800

    jdk21

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,10 @@ plugins {
     id("org.springframework.boot")
     id("io.spring.dependency-management")
     id("nu.studer.jooq")
-    `kotlin-dsl`
 }
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 repositories {
     mavenCentral()
