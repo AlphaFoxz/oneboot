87b4eacf59894989d72fc87281ddcda906c15610
AlphaFoxz <841958335@qq.com>
2024-08-28 21:56:45 +0800

    wip

--- app/src/main/java/com/github/alphafoxz/oneboot/app/configuration/AppConfiguration.java
+++ app/src/main/java/com/github/alphafoxz/oneboot/app/configuration/AppConfiguration.java
@@ -1,9 +1,13 @@
 package com.github.alphafoxz.oneboot.app.configuration;
 
-import com.github.alphafoxz.oneboot.core.configuration.CoreProperties;
-import jakarta.annotation.Resource;
+import cn.hutool.core.lang.Snowflake;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfiguration {
+    @Bean
+    public Snowflake snowflake() {
+        return new Snowflake();
+    }
 }
